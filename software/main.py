598a21bfbaf152a95c6529d9b4c0c961c30158c8
Harry Moy <harrymoy@me.com>
2016-02-19 08:24:27 +0000

    Setting up BaseHTTPServer

--- software/main.py
+++ software/main.py
@@ -1,1 +1,17 @@
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+import SimpleHTTPServer
 from dbHandler import dbHandler
+
+PORT_NUMBER = 8080
+
+class myHandler(BaseHTTPRequestHandler):
+	#Handle GET requests
+	def do_GET(self):
+		self.send_response(200)
+
+try:
+	server = HTTPServer(('', PORT_NUMBER), myHandler)
+	server.serve_forever()
+
+except KeyboardInterrupt:
+	server.socket.close()
