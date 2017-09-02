eb61ead5bf0a1b845eafe561ed46e2179a497aff
Harry Moy <harrymoy@me.com>
2017-09-02 13:50:04 +0100

    Added the gardenHandler file
    The gardenHandler file is what handles the majority of logic that the API uses. The only purpose of index.js is to route the API towards one of the handler's methods, from there the handler will do the majority of the legwork.

---
+++ node/bl/gardenHandler.js
@@ -0,0 +1,15 @@
+exports.getSoil = () => {
+  //Gets the soil data
+}
+
+exports.getTemperature = () => {
+  //Gets the temperature
+}
+
+exports.getLight = () => {
+  //Gets the light
+}
+
+exports.getPh = () => {
+  //Gets the ph
+}
